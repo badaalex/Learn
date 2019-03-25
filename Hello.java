913d73be3e825b7ad5e6d9dd07156dc44a6cc49b
badaalex <badaalex@yahoo.com>
2019-03-25 09:51:38 +0800

    test java again

--- Hello.java
+++ Hello.java
@@ -2,6 +2,6 @@ public class Hello{
 	public static void main(String[] args){
 		
 		for(int i=1; i<=10; i++)
-			System.out.println("HelloWorld ... " + i );
+			System.out.println("25 03 HelloWorld ... " + i );
 	}
 }
